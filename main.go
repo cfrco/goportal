4a40322e526cf12fcda769922cf59d98eb4303d7
CatCfrco <z82206.cat@gmail.com>
2013-09-08 12:56:55 +0800

    move core package to main

--- main.go
+++ main.go
@@ -9,8 +9,6 @@ import (
     "syscall"
 )
 
-import "github.com/cfrco/goportal/core"
-
 const (
     POSTFIX = ".goportal"
     ROOT_DIR_NAME = ".goportal"
@@ -59,7 +57,7 @@ func runReceiver(){
         return 
     }
 
-    receiver,err := core.NewReceiver(fifoName,core.MODE_RDWR)
+    receiver,err := NewReceiver(fifoName,MODE_RDWR)
     if err != nil {
         fmt.Println(err)
         return 
@@ -81,7 +79,7 @@ func runReceiver(){
         }
 
         if strings.HasPrefix(message,"#cmd:") {
-            err := core.RunInternalCommand(message)
+            err := RunInternalCommand(message)
             if err != nil {
                 if err.Error() == "command:end" {
                     break
@@ -89,7 +87,7 @@ func runReceiver(){
                 fmt.Println(err)
             }
         }else {
-            core.LastRet = core.CallSystem(message)
+            LastRet = CallSystem(message)
         }
     }
 }
@@ -99,7 +97,7 @@ func runSender(){
         return 
     }else{
         fifoName := fifoPath(flag.Arg(0)) 
-        sender,err := core.NewSender(fifoName)
+        sender,err := NewSender(fifoName)
         if err != nil {
             fmt.Println(err)
             return 
@@ -114,7 +112,7 @@ func runSender(){
                 cmdline = "#cmd:"+cmdline 
             }
         }else {
-            cmdline = core.ArgsToCmdline(flag.Args()[1:])
+            cmdline = ArgsToCmdline(flag.Args()[1:])
         }
         sender.SendMessage(cmdline)
     }
